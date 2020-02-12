55c4228b2ce28f3926b8c1c38f3d6b4e7bfcee2c
avraham <b.a.avraham@gmail.com>
2020-02-12 13:26:54 +0200

    meme working - add 3 lines

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ function initGrid() { }
 function initMemeGenerator() {
     let meme = getMeme();
     render(meme);
+    addline();
 }
 
 function render(meme) {
@@ -16,16 +17,18 @@ function render(meme) {
     resizeCanvas(ratio);
     img.onload = () => {
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
-        meme.linse.forEach((line, inx) => {
-            let x = canvas.width / 2, y = canvas.height / 2;
-            if (inx === 0 || inx === 1) y = (inx) ? canvas.height - 100 : 100;
+        meme.lines.forEach((line, inx) => {
             // ctx.lineWidth = '2'
             ctx.strokeStyle = line.colorStroke;
             ctx.fillStyle = line.colorfill;
             ctx.font = `${line.size}px Ariel`;
             ctx.textAlign = 'center'
-            ctx.fillText(line.text, x, y);
-            ctx.strokeText(line.text, x, y);
+            
+            line.height = ctx.measureText(line.text).actualBoundingBoxAscent;
+            line.width = ctx.measureText(line.text).width;
+            ctx.fillText(line.text, line.x, line.y);
+            ctx.strokeText(line.text, line.x, line.y);
+            
         });
     }
 
@@ -34,9 +37,28 @@ function render(meme) {
 function resizeCanvas(ratio) {
     let canvas = document.querySelector('#meme-canvas');
     let elContainer = document.querySelector('.canvas-container');
-    // Note: changing the canvas dimension this way clears the canvas
-
     elContainer.style.height = elContainer.offsetWidth / ratio + 'px'
     canvas.width = elContainer.offsetWidth;
     canvas.height = elContainer.offsetHeight;
+}
+
+
+function changeText(elText) {
+    changeTextLine(elText.value);
+    render(getMeme());
+}
+function addline() {
+    let memeLineCount = getMeme().lines.length;
+    let canvas = document.querySelector('#meme-canvas');
+    let y = canvas.height / 2;
+    if (memeLineCount === 0 || memeLineCount === 1) y = (memeLineCount) ? canvas.height - 100 : 100;
+    addLineService(canvas.width / 2, y)
+    document.querySelector('.edit-text').value = '';
+}
+
+function clickOnCanvas(ev) {
+    let { offsetX, offsetY } = ev;
+    let line = getLine(offsetX, offsetY);
+    document.querySelector('.edit-text').value = line.text;
+    
 }
--- js/model.js
+++ js/model.js
@@ -4,16 +4,8 @@ let gKeyword = {};
 let gImgs = createImges();
 let gMeme = {
     selectedImgId: 1,
-    seletedLineInx: 1,
-    linse: [
-        {
-            text: 'I learn JS',
-            size: 50,
-            align: 'left',
-            colorStroke: '#000000',
-            colorfill:'#ffffff'
-        }
-    ]
+    seletedLineInx: 0,
+    lines: []
 }
 
 function createImges() {
@@ -23,6 +15,32 @@ function createImges() {
 function getMeme() { return gMeme }
 
 function getImgUrl(id) {
-    let img =gImgs.find(img => id === img.id)
+    let img = gImgs.find(img => id === img.id)
     return img.url;
+}
+
+function changeTextLine(text) {
+    gMeme.lines[gMeme.seletedLineInx].text = text;
+}
+
+function addLineService(x, y) {
+    gMeme.seletedLineInx = gMeme.lines.length;
+    gMeme.lines.push({
+        text: '',
+        size: 50,
+        colorStroke: '#000000',
+        colorfill: '#ffffff',
+        x, y, width: 0, height: 0
+    })
+}
+
+function getLine(x, y) {
+    let lineInx = gMeme.lines.findIndex(line =>
+        x < line.x + line.width / 2 &&
+        x > line.x - line.width / 2 &&
+        y < line.y + line.height * (2 / 5) &&
+        y > line.y - line.height)
+    gMeme.seletedLineInx = lineInx;
+    return gMeme.lines[lineInx];
+
 }
