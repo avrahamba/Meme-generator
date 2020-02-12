86a3c96654e6f93cde05b3a3aa4763eb92cfe80e
avraham <b.a.avraham@gmail.com>
2020-02-12 11:38:19 +0200

    test

--- js/main.js
+++ js/main.js
@@ -11,22 +11,24 @@ function render(meme) {
     let canvas = document.querySelector('#meme-canvas');
     let ctx = canvas.getContext('2d');
     let img = new Image()
-    img.src = `../images/full/${getImgUrl(meme.selectedImgId)}`;
+    img.src = `images/full/${getImgUrl(meme.selectedImgId)}`;
     let ratio = img.height / img.width;
     resizeCanvas(ratio);
     img.onload = () => {
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
-        meme.linse.forEach(line => {
+        meme.linse.forEach((line, inx) => {
+            let x = canvas.width / 2, y = canvas.height / 2;
+            if (inx === 0 || inx === 1) y = (inx) ? canvas.height - 100 : 100;
             // ctx.lineWidth = '2'
             ctx.strokeStyle = line.colorStroke;
             ctx.fillStyle = line.colorfill;
             ctx.font = `${line.size}px Ariel`;
-            // ctx.textAlign = 'center'
-            ctx.fillText(line.text, 100, 100);
-            ctx.strokeText(line.text, 100, 100);
+            ctx.textAlign = 'center'
+            ctx.fillText(line.text, x, y);
+            ctx.strokeText(line.text, x, y);
         });
     }
-        
+
 }
 
 function resizeCanvas(ratio) {
