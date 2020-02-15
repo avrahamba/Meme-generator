03cd4a7444add3b456e6c626755c8be56dfecaf9
avraham <b.a.avraham@gmail.com>
2020-02-15 22:21:33 +0200

    share-api

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ function init() {
             url: 'https://developer.mozilla.org',
           });
         });
-
+alert('')
     }else{
         document.querySelector('.share-api').remove();
     }
